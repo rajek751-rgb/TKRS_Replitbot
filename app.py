b8466e65f25f4f393fd28295fd3f1ca8188bfdcb
rajek751-rgb <rajek751@gmail.com>
2026-02-23 19:04:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,14 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
-PORT = int(os.environ.get("PORT", 10000))
-RENDER_EXTERNAL_URL = os.getenv("RENDER_EXTERNAL_URL")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ Бот работает стабильно!")
+    await update.message.reply_text("✅ TKRS Bot работает стабильно!")
 
 def main():
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-
-    app.run_webhook(
-        listen="0.0.0.0",
-        port=PORT,
-        webhook_url=f"{RENDER_EXTERNAL_URL}/{TOKEN}"
-    )
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
