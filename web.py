f07fb674a1344f7ed417929c1564eee19d76bfbf
rajek751-rgb <rajek751@gmail.com>
2026-03-09 19:44:17 +0500

    Create web.py

---
+++ web.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from database import cursor
+
+app = Flask(__name__)
+
+@app.route("/")
+def panel():
+
+    cursor.execute("""
+    SELECT username,date,done,problems,plan
+    FROM reports
+    ORDER BY id DESC
+    LIMIT 100
+    """)
+
+    rows = cursor.fetchall()
+
+    html = "<h2>📊 Reports CRM</h2><table border=1>"
+
+    html += "<tr><th>User</th><th>Date</th><th>Done</th><th>Problems</th><th>Plan</th></tr>"
+
+    for r in rows:
+
+        html += f"""
+        <tr>
+        <td>@{r[0]}</td>
+        <td>{r[1]}</td>
+        <td>{r[2]}</td>
+        <td>{r[3]}</td>
+        <td>{r[4]}</td>
+        </tr>
+        """
+
+    html += "</table>"
+
+    return html
